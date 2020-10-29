550a2cad8fdf69c56e6c3c7fbcc0045b1755550d
jamesmp88 <jamesmprice@hotmail.co.uk>
2020-10-29 13:35:26 +0000

    Updates

---
+++ precache-manifest.f17f263f5be557a7e028fe46304a77f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e4620501df96eb3d60d0dbea35c9db7",
+    "url": "/foodfinder/index.html"
+  },
+  {
+    "revision": "eafa42dcf7911af015c7",
+    "url": "/foodfinder/static/css/main.84c6b007.chunk.css"
+  },
+  {
+    "revision": "39386279d7103dc89487",
+    "url": "/foodfinder/static/js/2.f4b03e9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/foodfinder/static/js/2.f4b03e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eafa42dcf7911af015c7",
+    "url": "/foodfinder/static/js/main.e3b818bc.chunk.js"
+  },
+  {
+    "revision": "01ac75e46e021c27a614",
+    "url": "/foodfinder/static/js/runtime-main.35f828dc.js"
+  },
+  {
+    "revision": "ba7b8a49d132288bbc5fe66e3b685c08",
+    "url": "/foodfinder/static/media/logo.ba7b8a49.svg"
+  },
+  {
+    "revision": "802968318a7e570d8fe1c1c458c97b07",
+    "url": "/foodfinder/static/media/main.80296831.jpg"
+  }
+]);
